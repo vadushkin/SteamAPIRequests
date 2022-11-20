dac9d25f84acaa9d0041e8865e3d1a39170f780f
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-11-20 17:07:20 +0500

    added: get_friend_list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import requests
 
@@ -14,8 +15,8 @@ def get_player_summaries(user_id: int) -> [dict, None]:
     :user_id <int>: Player ID.
     """
 
-    url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/' \
-          f'?key={settings.key}&' \
+    url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?' \
+          f'key={settings.key}&' \
           f'steamids={user_id}'
 
     response = requests.get(url)
@@ -39,7 +40,7 @@ def get_player_summaries(user_id: int) -> [dict, None]:
     return response.json()
 
 
-def get_players_summaries(user_ids: list[int]) -> list:
+def get_players_summaries(user_ids: list[int]) -> list[dict]:
     """
     Return a list of information about people.
 
@@ -49,8 +50,8 @@ def get_players_summaries(user_ids: list[int]) -> list:
     lst_users = []
 
     for user_id in user_ids:
-        url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/' \
-              f'?key={settings.key}&' \
+        url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?' \
+              f'key={settings.key}&' \
               f'steamids={user_id}'
 
         response = requests.get(url)
@@ -63,13 +64,13 @@ def get_players_summaries(user_ids: list[int]) -> list:
             logger.warning(f'User - {user_id}. There is no such user')
             continue
 
-        lst_users.append(response.json()['response']['players'][0].items())
+        lst_users.append(response.json())
 
-    for lst_user in lst_users:
-        for user in lst_user:
+        for user in response.json()['response']['players'][0].items():
             print(user)
-
         print()
+
+        time.sleep(0.5)
 
     return lst_users
 
@@ -80,8 +81,8 @@ def get_global_achievement_percentages_for_app(game_id: int) -> [dict, None]:
 
     :game_id <int>: Game ID.
     """
-    url = f'https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/' \
-          f'?gameid={game_id}&' \
+    url = f'https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?' \
+          f'gameid={game_id}&' \
           f'format=json'
 
     response = requests.get(url)
@@ -101,7 +102,7 @@ def get_global_achievement_percentages_for_app(game_id: int) -> [dict, None]:
     return response.json()
 
 
-def get_news_for_app(app_id: int, count: int, max_length: int) -> [list, None]:
+def get_news_for_app(app_id: int, count: int, max_length: int) -> [dict, None]:
     """
     Return the latest of a game specified by its appID.
 
@@ -110,8 +111,8 @@ def get_news_for_app(app_id: int, count: int, max_length: int) -> [list, None]:
     :max_length <int>: Maximum length of each news entry.
     """
 
-    url = f'https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/' \
-          f'?appid={app_id}&' \
+    url = f'https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?' \
+          f'appid={app_id}&' \
           f'count={count}&' \
           f'maxlength={max_length}&' \
           f'format=json'
@@ -129,15 +130,47 @@ def get_news_for_app(app_id: int, count: int, max_length: int) -> [list, None]:
 
         print()
 
-    return response.json()['appnews']['newsitems']
+    return response.json()
+
+
+def get_friend_list(steam_id: int, relationship: str) -> None:
+    """
+    Returns the friend list of any Steam user, provided their Steam Community profile visibility is set to “Public”.
+
+    :steam_id <int>: User ID.
+    :relationship <str>: Relationship filter. Possibles values: all, friend.
+    """
+
+    url = f'https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?' \
+          f'key={settings.key}&' \
+          f'steamid={steam_id}&' \
+          f'relationship={relationship}'
+
+    response = requests.get(url)
+
+    if response.status_code > 400:
+        logger.warning(f'Key - {str(settings.key)[:6]}... '
+                       f'Steam ID - {steam_id}. '
+                       f'Relationship - {relationship}. '
+                       f'Status code - {response.status_code}.')
+        return None
+
+    for friends in response.json()['friendslist']['friends']:
+        for friend in friends.items():
+            print(friend)
+
+        print()
+
+    return response.json()
 
 
 def main():
     try:
         # get_player_summaries(76561197963562688)
-        # get_players_summaries([76561199087475515, 76561198331785567])
+        # get_players_summaries([76561197963562688, 76561197963562688])
         # get_global_achievement_percentages_for_app(440)
         # get_news_for_app(440, 3, 300)
+        # get_friend_list(76561197960435530, 'all')
         pass
 
     except Exception as _ex:
